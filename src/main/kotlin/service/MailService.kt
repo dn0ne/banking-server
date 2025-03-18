2aa4302be05a5223c303fa43e7dd869246807e46
dn0ne <mmmdanone04@gmail.com>
2025-03-18 14:45:27 +0200

    put mail server data in a separate file

--- src/main/kotlin/service/MailService.kt
+++ src/main/kotlin/service/MailService.kt
@@ -1,19 +1,37 @@
 package com.dn0ne.service
 
 import com.dn0ne.model.user.User
+import io.github.cdimascio.dotenv.dotenv
 import org.simplejavamail.email.EmailBuilder
 import org.simplejavamail.mailer.MailerBuilder
 import org.slf4j.LoggerFactory
+
+private val dotenv = dotenv {
+    directory = "./"
+    filename = "mail.env"
+}
+
+private val mailHost = dotenv.get("MAIL_HOST")
+    ?: throw IllegalArgumentException("Missing MAIL_HOST environment variable")
+
+private val mailPort = dotenv.get("MAIL_PORT")?.toIntOrNull()
+    ?: throw IllegalArgumentException("Missing MAIL_PORT environment variable")
+
+private val mailUsername = dotenv.get("MAIL_USERNAME")
+    ?: throw IllegalArgumentException("Missing MAIL_USERNAME environment variable")
+
+private val mailPassword = dotenv.get("MAIL_PASSWORD")
+    ?: throw IllegalArgumentException("Missing MAIL_PASSWORD environment variable")
 
 class MailService {
     private val logger = LoggerFactory.getLogger(MailService::class.java)
 
     private val mailer = MailerBuilder
         .withSMTPServer(
-            "smtp.gmail.com",
-            587,
-            "dev.dn0ne@gmail.com",
-            "tuvh ttvf krnc tbay"
+            mailHost,
+            mailPort,
+            mailUsername,
+            mailPassword
         )
         .buildMailer()
 
